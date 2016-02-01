038ef0b3f2f3fb125a5fc325359952312a456a38
sherbert250 <jtrayer1@asu.edu>
2016-01-31 22:37:27 -0700

    desks, users, and walls can all be added, dragged, and dropped. Rotation is currently having issues. will continue to work on this. Also, if attempt to run this, please note that 'npm start' seems to like to get rid of the html view. These files willed be rafactored later to get rid of the inline css, stylesheet addition, and other things that should belong elsewhere

--- src/controllers/CreateSeatingController.js
+++ src/controllers/CreateSeatingController.js
@@ -8,34 +8,77 @@ import env from '../core/env';
 //
 
 export default ['$http', '$scope', ($http, $scope) => {
-  var element = document.getElementById('grid-snap'),
-    x = 0, y = 0;
 
-interact(element)
-  .draggable({
-    snap: {
-      targets: [
-        interact.createSnapGrid({ x: 30, y: 30 })
-      ],
-      range: Infinity,
-      relativePoints: [ { x: 0, y: 0 } ]
-    },
-    inertia: true,
-    restrict: {
-      restriction: element.parentNode,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-      endOnly: true
-    }
-  })
-  .on('dragmove', function (event) {
-    x += event.dx;
-    y += event.dy;
+  $scope.createWall = function() {
+    $('<div class="draggable drag"><i class="fa fa-stop fa-2x default"></i><i class="fa fa-stop fa-2x default"></i><i class="fa fa-stop fa-2x default"></i></div>').appendTo(".live-demo");
+  }
 
-    event.target.style.webkitTransform =
-    event.target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
-  });
-  
+  $scope.createDesk = function() {
+    $('<div class="draggable drag"><i class="fa fa-simplybuilt fa-3x info"></i></div>').appendTo(".live-demo");
+  }
+
+  $scope.createUser = function() {
+    $('<div class="draggable drag"><i class="fa fa-user fa-3x danger"></i></div>').appendTo(".live-demo");
+  }
+
+  // target elements with the "draggable" class
+  interact('.draggable')
+    .on('doubletap', function(event) {
+      event.currentTarget.classList.toggle('rotate');
+    })
+    .draggable({
+      snap: {
+        targets: [
+          interact.createSnapGrid({
+            x: 30,
+            y: 30
+          })
+        ],
+        range: Infinity,
+        relativePoints: [{
+          x: 0,
+          y: 0
+        }]
+      },
+      // enable inertial throwing
+      inertia: true,
+      // keep the element within the area of it's parent
+      restrict: {
+        restriction: "parent",
+        endOnly: true,
+        elementRect: {
+          top: 0,
+          left: 0,
+          bottom: 1,
+          right: 1
+        }
+      },
+      // enable autoScroll
+      autoScroll: true,
+
+      // call this function on every dragmove event
+      onmove: dragMoveListener
+    });
+
+  function dragMoveListener(event) {
+    var target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
+
   // $scope.collection = teamMembers;
   // $http({
   //   method: 'GET',
