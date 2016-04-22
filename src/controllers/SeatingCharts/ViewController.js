e881ab5f750d3134c8ee24475925fdcf7d960a46
garrettgutierrezasu <garrettdgutierrez@gmail.com>
2016-04-22 02:56:00 -0700

    Fixed views for floor plan and seating chart

--- src/controllers/SeatingCharts/ViewController.js
+++ src/controllers/SeatingCharts/ViewController.js
@@ -16,7 +16,6 @@ export default ['$scope', '$http', '$location', '$routeParams', '$window', ($sco
 
   // get the id from url params
   const id = $routeParams.id;
-
   // fetch the seating chart
   $scope.api.fetchSeatingChart(id, function(err, response) {
     if (err) {
@@ -29,44 +28,69 @@ export default ['$scope', '$http', '$location', '$routeParams', '$window', ($sco
     }
     $scope.seatingChart = response.data[0];
 
-    //
-    // Now, load everything we need for React app
-    //
-
-    // add global data
-    $window.ISC = {
-      initialState: {
-        design: {
-          cols: $scope.seatingChart.base_floor_plan_cols,
-          name: $scope.seatingChart.name,
-          rows: $scope.seatingChart.base_floor_plan_rows,
-          spots: $scope.seatingChart.seating_chart ? JSON.parse($scope.seatingChart.seating_chart) : $scope.seatingChart.base_floor_plan ? JSON.parse($scope.seatingChart.base_floor_plan) : undefined
-        },
-        settings: {
-          readOnly: true
+    // fetch the office employees
+    $scope.api.fetchOfficeEmployees($scope.seatingChart.office_id, function (err, result) {
+      if (err) {
+        return log(err);
+      } else {
+        var seating_chart_JSON = $scope.seatingChart.seating_chart ? JSON.parse($scope.seatingChart.seating_chart) : [];
+        $scope.officeEmployees = result.data;
+        for (var i in seating_chart_JSON) {
+          //console.log(seating_chart_JSON[i]);
+          for (var j in seating_chart_JSON[i]) {
+            for (var k = 0; k < $scope.officeEmployees.length; k++) {
+              if ($scope.officeEmployees[k].employeeID == seating_chart_JSON[i][j].userId) {
+                seating_chart_JSON[i][j].name = $scope.officeEmployees[k].firstName + ' ' + $scope.officeEmployees[k].lastName;
+                console.log(seating_chart_JSON[i][j]);
+              }
+            }
+          }
         }
-      },
-      onExit() {
-        window.location.replace(window.location.origin + '/seating-charts');
-        // $window.history.back();
       }
-    };
 
-    $scope.addCssToHead([
-      'https://fonts.googleapis.com/icon?family=Material+Icons',
-      'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css'
-    ]);
+      //
+      // To do: put seating_chart_JSON into spots and have the seating chart application read the attribute name instead of the userID
+      //
 
-    // add materialize js
-    var materializeTag = document.createElement('script');
-    materializeTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js';
-    document.body.appendChild(materializeTag);
+      //
+      // Now, load everything we need for React app
+      //
 
-    // add bundle after materializeTag
-    materializeTag.onload = function() {
-      var bundleTag = document.createElement('script');
-      bundleTag.src = '/js/designer.js';
-      document.body.appendChild(bundleTag);
-    };
+      // add global data
+      $window.ISC = {
+        initialState: {
+          design: {
+            cols: $scope.seatingChart.base_floor_plan_cols,
+            name: $scope.seatingChart.name,
+            rows: $scope.seatingChart.base_floor_plan_rows,
+            spots: $scope.seatingChart.seating_chart ? JSON.parse($scope.seatingChart.seating_chart) : $scope.seatingChart.base_floor_plan ? JSON.parse($scope.seatingChart.base_floor_plan) : undefined
+          },
+          settings: {
+            readOnly: true
+          }
+        },
+        onExit() {
+          window.location.replace(window.location.origin + '/seating-charts');
+          // $window.history.back();
+        }
+      };
+
+      $scope.addCssToHead([
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css'
+      ]);
+
+      // add materialize js
+      var materializeTag = document.createElement('script');
+      materializeTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js';
+      document.body.appendChild(materializeTag);
+
+      // add bundle after materializeTag
+      materializeTag.onload = function() {
+        var bundleTag = document.createElement('script');
+        bundleTag.src = '/js/designer.js';
+        document.body.appendChild(bundleTag);
+      };
+    });
   });
 }];
